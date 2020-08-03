443165649e69b89ae0f025cd06392b03d4eb3d11
davds <davidmarquesstone@gmail.com>
2020-08-03 14:32:22 -0400

    - Added missing semicolon

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const hostname = "localhost";
-const classes = require("./classes.js")
+const classes = require("./classes.js");
 
 app.use(express.json());
 app.use(express.static("public_html"));

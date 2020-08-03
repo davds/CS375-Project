b6a484e37821f1d87cfcc58a395856213e0290c2
davds <davidmarquesstone@gmail.com>
2020-08-03 14:06:25 -0400

    - Removed unneeded reference

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const { table } = require("console");
 const app = express();
 const port = 3000;
 const hostname = "localhost";
 
 app.use(express.json());
+app.use(express.static("public_html"));
 
 app.get("/", function (req,res) {
 	res.status(200);
 	res.setHeader('Content-Type', 'text/html');
 
-  let dims = [50, 25];
+  /*let dims = [50, 25];
   let tableBoard = "";
   for (let i = 0; i < dims[1]; i++) {
     tableBoard += "<tr>";
@@ -18,8 +18,21 @@ app.get("/", function (req,res) {
       tableBoard += "<td></td>";
     }
     tableBoard += "</tr>";
-  }
+  }*/
 
+  scripts = `<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+            <script src="conway.js"></script>
+            
+            <script>
+              initDisplayBoard();
+              $('td').on('click', (e) => {
+                $(e.target).toggleClass("dead");
+                $(e.target).toggleClass("alive");
+              }); 
+              $('#go').on('click', (e) => {
+                startSim();
+              });
+            </script>`
 
 	let html = `<!DOCTYPE html>
 				<html>
@@ -44,15 +57,19 @@ app.get("/", function (req,res) {
             }
 						table, th, td {
 							border: 1px solid black;
-						}
-						.high-usage {
-							background-color: red;
-						}
+						}                    
+            .alive {
+              background-color: #b860e0;
+            }
+            .dead {
+              background-color: white;
+            }
 					</style>
 				<body>
-					<table style="margin: auto">
-						${tableBoard}
-					</table>
+					<table id="game-of-life" style="margin: auto">
+          </table>
+          <input id="go" type="button" value="Go!" />
+          ${scripts}
 				</body>
 				</html>`;
 	

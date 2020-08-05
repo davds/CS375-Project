24e754d448b9549f4f9517b50b8c695bd8722a2f
davds <davidmarquesstone@gmail.com>
2020-08-04 21:09:44 -0400

    - Resolved conflicts

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const env = require("./env.json");
 const app = express();
 const {Player, ActivePiece} = require("./classes.js");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public_html"));

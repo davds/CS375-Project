898b1321abdba3cf9ae15f06de094e45bc6cc98c
linli56304 <linli56304@gmail.com>
2020-08-19 18:52:43 -0400

    made blinker and square

--- public_html/shared.js
+++ public_html/shared.js
@@ -1,4 +1,23 @@
 (function (exports) { // https://caolan.uk/articles/writing-for-node-and-the-browser/
+  //Precondish: [blinkerPos[0], blinkerPos[1]] is the center of the blinker
+  //Postcondish: returns a horizontal blinker
+  function makeBlinkerPos(blinkerPos) {
+    positions = [
+      [blinkerPos[0], blinkerPos[1]],
+      [blinkerPos[0] - 1, blinkerPos[1]],
+      [blinkerPos[0] + 1, blinkerPos[1]],
+    ];
+    return positions;
+  }
+  function makeSquarePos(blinkerPos) {
+    positions = [
+      [blinkerPos[0], blinkerPos[1]],
+      [blinkerPos[0] - 1, blinkerPos[1]],
+      [blinkerPos[0] - 1, blinkerPos[1] + 1],
+      [blinkerPos[0], blinkerPos[1] + 1],
+    ];
+    return positions;
+  }
   //Precondish: duble with x, y coords of center of a glider, a string representing orientation of glider
   //Postcondish: returns positions of cells needed to make glider
   function makeGliderPos(gliderPos, orientation) {

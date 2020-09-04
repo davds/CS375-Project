e974e596bc30584aaeee58e5eb6b53c6fee9af4d
davds <davidmarquesstone@gmail.com>
2020-09-03 21:50:10 -0400

    - styling

--- public_html/game.js
+++ public_html/game.js
@@ -563,7 +563,7 @@ function gameOver() {
 function addMessage(id, message, style, joining) {
     let display = document.getElementById("displayingMessage");
     let div = document.createElement('div');
-    div.innerHTML =`<span style="${style}">${id}</span>`;
+    div.innerHTML =`<span style="font-weight: bold; ${style}">${id}</span>`;
     div.innerHTML += joining ? " " : ": ";
     div.innerHTML += message;
     display.append(div);
